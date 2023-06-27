46ae66dd636ddbd69ea4a5a294df861d2b90843c
Gyansri K <koirala.gyansri7@gmail.com>
2023-06-27 09:33:43 -0600

    Initial Commit

---
+++ testRun/testRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRunApp.swift
+//  testRun
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct testRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
